bff8a4cdd381526aa962d2ba36ea840bc83bd3f0
unknown <marcus.larsen.swe@gmail.com>
2021-11-18 23:09:37 +0100

    soon

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import LoginUserX from './loginUserX';
 import LogOffUserX from './logoffUserX';
 import UpVoteX from './upVoteX';
 import DownVoteX from './downVoteX';
+import PostSida from './postSida';
 import TestXX from './testX';
 import TestYY from './testY';
 
@@ -16,7 +17,11 @@ function App() {
   const [token, setToken] = useState("");
   const [posts, setPosts] = useState([]);
   const [omegaHook, callOmegaHook] = useState(true);
+  const [sida, setSida] = useState(1);  
+  const [sendPost, setSendPost] = useState();
+  const [idComment, setIdComment]= useState();
   let background = false;
+  
 
   const yesGo = () => {
     callOmegaHook(!omegaHook);
@@ -51,27 +56,75 @@ function cutMessage(y){
 }
 
 
+
+function topMenu(){
   return (
-    <div >
-      <div className="fontX"><a href="https://fontmeme.com/graffiti-creator/"><img src="https://fontmeme.com/permalink/211116/662e47978ea0ad4aa807207649683392.png" 
-      alt="graffiti-creator" border="0"/></a>
-      <b className="moveUp"> Name: <input type="text" name="username" onChange={e => {setUser(e.target.value)}}/>
-        Password: <input type="text" name="username" onChange={e => {setPassword(e.target.value)}}/>
-        <LoginUserX setTok={setToken} setLogIn={setLoggedIn} user={user} password={password}/>
-        <button className="fontX" onClick={e =>{AddUserX(user, password)}}> Register </button>  
-        <LogOffUserX token={token} user={user} setTok={setToken} setLogIn={setLoggedIn}/>    
-        {loggedIn}    
-      </b>  
-      </div>     
+    <div className="fontX"><img src="https://fontmeme.com/permalink/211116/662e47978ea0ad4aa807207649683392.png" 
+    alt="graffiti-creator" border="0" onClick={e =>{setSida(1)}}/>
+    <b className="moveUp"> Name: <input type="text" name="username" onChange={e => {setUser(e.target.value)}}/>
+      Password: <input type="text" name="username" onChange={e => {setPassword(e.target.value)}}/>
+      <LoginUserX setTok={setToken} setLogIn={setLoggedIn} user={user} password={password}/>
+      <button className="fontX" onClick={e =>{AddUserX(user, password)}}> Register </button>  
+      <LogOffUserX token={token} user={user} setTok={setToken} setLogIn={setLoggedIn}/>    
+      {loggedIn}    
+    </b>  
+    </div>    
+  )
+}
+
+function ChooseToPrint(){ 
+  switch(sida){
+    case 1:
+      { 
+        return PrintaMainSida(); }      
+    case 2:
+      { 
+        return PrintaPostSida();}
+    case 3:
+      { 
+        return PrintaNewPost();}
+    default:{     
+      return PrintaMainSida(); }
+  }
+}
+
+function PrintaMainSida(){
+  return  ( <div>
+      {topMenu()}
       {posts.map((p,index) => <div key={p.id} index={index} className={changeBackgroundClassName()}><b className="voteSize">{p.upvote}</b>      
-        <UpVoteX token={token} id={p.id} yesGo={yesGo}/>       
-        <b className="voteSize"> {p.downvote} </b>
-        <DownVoteX token={token} id={p.id} yesGo={yesGo}/>
-        <b className="titleX">{p.title} </b>  <b className="authorX">{p.author}</b> {cutMessage(p.message)}
-       <button className="readButton" onClick={e =>{}}> r e a d </button>
-       </div>)}       
-    </div>
+      <UpVoteX token={token} id={p.id} yesGo={yesGo}/>       
+      <b className="voteSize"> {p.downvote} </b>
+      <DownVoteX token={token} id={p.id} yesGo={yesGo}/>
+      <b className="titleX">{p.title} </b>  <b className="authorX">{p.author}</b> {cutMessage(p.message)} <span className="dateY">{p.date}</span>
+   
+    <button className="readButton" onClick={e =>{setSendPost(p); setIdComment(p.id);  setSida(2)}}> r e a d </button>
+
+   </div>)}       
+  </div>
   );
+}
+
+function PrintaPostSida(){
+
+  console.log(idComment);
+  console.log(sendPost);
+    return ( <div> 
+        {topMenu()}
+        <PostSida sendPost={sendPost} token={token} id={idComment} user={user} />
+        </div>      
+    )
+}
+
+function PrintaNewPost(){
+
+}
+
+
+  return (<div>{ChooseToPrint()}</div> 
+  )
+
+
+
 }
 
 export default App;
--- src/upVoteX.js
+++ src/upVoteX.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './App.css';
 
 export default function upVote(props) {
-    
+ 
     async function fetchUpVote(){
         const resp = await fetch('http://localhost:8080/post/voteup', {
           method: 'PUT',
